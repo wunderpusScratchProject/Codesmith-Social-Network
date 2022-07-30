29c1e071fb5261de3a58b399d124aacfc6735f40
Nick Krug <n.e.krug@gmail.com>
2022-07-30 15:39:25 -0400

    Minor syntax fixes

--- server/controllers/oauthController.js
+++ server/controllers/oauthController.js
@@ -1,4 +1,3 @@
-
 const CLIENT_ID = '78jexcndblghpj';
 const REDIRECT_URI = 'https%3A%2F%2Flocalhost%3A8080%2Fhome';
 const SCOPE = 'r_liteprofile';
@@ -7,9 +6,9 @@ const oauthController = {};
 // TODO: Refactor this route to use our NODE_ENV to point to localhost:3000 if production and 8080 if dev. Then switch localhost:3000 again once we get the site hosted.
 // TODO: Make actual error handling
 // TODO (stretch feature, to prevent CSRF attacks, which don't matter on our site, since a malicious actor can't do anything except mess with the user profile a bit): Generate unique state and store it in use cookies. https://auth0.com/docs/secure/attack-protection/state-parameters
- oauthController.auth = async (req, res, next) => {
+oauthController.auth = async (req, res, next) => {
   try {
-    let res = await fetch(`https://www.linkedin.com/oauth/v2/authorization/
+    const res = await fetch(`https://www.linkedin.com/oauth/v2/authorization/
       ?response_type="code"
       &client_id=${CLIENT_ID}
       &redirect_uri=${REDIRECT_URI}
