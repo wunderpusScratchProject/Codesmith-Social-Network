94ae8655ca40271db947d0cece63c0afe5484a4d
Nick Krug <n.e.krug@gmail.com>
2022-08-01 06:46:26 -0400

    Working code for getting to the OAuth page

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+const CLIENT_ID = '78jexcndblghpj';
+const REDIRECT_URI = 'https%3A%2F%2Flocalhost%3A8080%2Fhome';
+const SCOPE = 'r_liteprofile';
+
 export const LandingPage = (props) => {
 
   //Redirect user to LinkedIn OAuth then if successful set authenticated to true
@@ -12,7 +16,10 @@ export const LandingPage = (props) => {
   return (
     <div className="LandingPage">
       LandingPage
-      <button className="LogInButton" onClick={logIn}></button>
+      <a href={`https://www.linkedin.com/oauth/v2/authorization/?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state="A9Sd.udf8-d1"&scope=${SCOPE}`}>
+        <img src="../../server/assets/linkedin-signin-button.png"/>
+      </a>
+      {/* <button className="LogInButton" onClick={logIn}></button> */}
     </div>
   );
 };
